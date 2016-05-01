7fe6e50435da1403e526b37bbb34d4d82d8a3942
Gilmar Andrade <gilmar.victhur@hotmail.com>
2016-05-01 13:51:42 -0300

    Refatoração checkbox e radiobutton

---
+++ gerarrifaStyleGuide/public_html/js/widgets.js
@@ -0,0 +1,50 @@
+function initCheckbox() {
+    $(".checkbox_input").change(function() {
+        $(this).parent().toggleClass("is-checked");
+    });
+    
+    updateCheckbox();
+}
+
+function initRadiobutton() {
+    $(".radiobutton_input").on("change", function() {
+        console.log('radiobutton changed');
+        if ($(this).prop("checked")) {
+            var name = $(this).attr("name");
+            $("input[name='" + name + "']").parent().removeClass("is-checked");
+            $(this).parent().addClass("is-checked");
+        }
+    });
+    
+    updateRadiobutton();
+}
+
+function updateCheckbox() {
+    $(".checkbox_input").each(function() {
+        if ($(this).prop("checked")) {
+            $(this).parent().addClass("is-checked");
+        }else{
+            $(this).parent().removeClass("is-checked");
+        }
+        
+        if ($(this).prop("disabled")) {
+            $(this).parent().addClass("is-disabled");
+        } else {
+            $(this).parent().removeClass("is-disabled");
+        }
+    });
+}
+
+function updateRadiobutton() {
+    $("input[type='radio']").parent().removeClass("is-checked");
+    $(".radiobutton_input").each(function() {
+        if ($(this).prop("checked")) {
+            $(this).parent().addClass("is-checked");
+        }
+        if ($(this).prop("disabled")) {
+            $(this).parent().addClass("is-disabled");
+        } else {
+            $(this).parent().removeClass("is-disabled");
+        }
+    });
+}
